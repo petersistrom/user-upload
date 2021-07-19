f2285c5fac385afb8d9e48d1fb6ef9257c34b223
petersistrom <peter.sistrom@gmail.com>
2021-07-18 23:37:21 -0700

    user_upload updated

--- user_upload.php
+++ user_upload.php
@@ -4,6 +4,7 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 //
 include ('help.php');
+include ('CSVParser.php');
 
 $shortopts  = "u::"; //username
 $shortopts .= "p::"; //password
@@ -19,19 +20,21 @@ $longopts  = array(
 $options = getopt($shortopts, $longopts);
 $dry_run = false;
 
-if (array_key_exists("help", $options)){
+if (array_key_exists("help", $options)){//if help is an argument show help and do nothing else
   showHelp();
   die;
 }
 
-if(!array_key_exists("file", $options)){
-  echo "ERROR - No file, please include a CSV file as an argument using the --file flag\n";
+if(!array_key_exists("file", $options)){//a file always needs to be passed as an argument
+  fwrite(STDOUT, "ERROR - No file, please include a CSV file as an argument using the --file flag\n");
   showHelp();
   die;
 }
 
-if(array_key_exists("dry_run", $options)){
-  $dry_run = true;
+if(array_key_exists("dry_run", $options)){//dry run no database calls
+  $csvParser = new CSVParser();
+  $csvParser->parseCSV($options['file']);
+  fwrite(STDOUT, "CSV succesfully parsed in dry run mode ".count($csvParser->getUsers())." valid users in CSV\n");
 }
 
 
